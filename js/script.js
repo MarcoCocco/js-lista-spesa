6d058e6c4be2cce826fdb60140a20c8ffb2857ae
MarcoCocco <marcococco28@gmail.com>
2023-03-01 19:20:28 +0100

    feat: aggiunto un piccolo bonus per aggiungere un elemento alla lista

--- js/script.js
+++ js/script.js
@@ -20,28 +20,40 @@ while (index < listaSpesa.length) {
 let listaSpesa = ['Uova', 'Latte', 'Pasta', 'Carta igienica', 'Frutta', 'Verdura', 'Acqua'];
 
 let listItemsEl = document.getElementById('list-items');
+let addButtonEl = document.getElementById('add-button');
 let listButtonEl = document.getElementById('list-button');
+
+let addItemEl = document.getElementById('addItem');
 
 //2. Creare una variabile indice per tenere traccia dell'elemento corrente
 let index = 0;
 
+//Piccolo bonus per aggiungere un elemento alla lista
+addButtonEl.addEventListener('click', function () {
 
-listButtonEl.addEventListener('click', function () {
+    listaSpesa.push(addItemEl.value);
 
-    //3. Creare un ciclo while per eseguire le seguenti operazioni, finché l'indice è minore della lunghezza dell'array:
+});
+
+listButtonEl.addEventListener('click', function(){
+
     while (index < listaSpesa.length) {
 
         //   a. Ottenere l'elemento corrente dall'array utilizzando l'indice
         let currentItem = listaSpesa[index];
-
+    
         //   b. Stampare l'elemento corrente sulla pagina
         let newLiEl = document.createElement('li');
         newLiEl.textContent = currentItem;
         listItemsEl.append(newLiEl);
-
+    
         //   c. Incrementare l'indice di 1
         index++;
-
+    
     }
 
 });
+
+
+//3. Creare un ciclo while per eseguire le seguenti operazioni, finché l'indice è minore della lunghezza dell'array:
+
