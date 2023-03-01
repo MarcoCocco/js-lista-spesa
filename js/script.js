9e444a2e6c2bf52f571f774d57e8bd9377ba6723
MarcoCocco <marcococco28@gmail.com>
2023-03-01 16:57:15 +0100

    feat: creato uno pseudo codice dell'esercizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//1. Creare un array di elementi della lista della spesa
+//2. Creare una variabile indice per tenere traccia dell'elemento corrente
+//3. Creare un ciclo while per eseguire le seguenti operazioni, finché l'indice è minore della lunghezza dell'array:
+//   a. Ottenere l'elemento corrente dall'array utilizzando l'indice
+//   b. Stampare l'elemento corrente sulla pagina
+//   c. Incrementare l'indice di 1
+
+let listaSpesa = ['Uova', 'Latte', 'Pasta', 'Carta igienica'];
+
+let index = 0;
+
+while (index < listaSpesa.length) {
+
+    console.log(listaSpesa[index]);
+
+    index++;
+}
